ee2279f12402cf1658065003ccdf8a1ef9f574f3
Kudret <kudret.kustul.pro@gmail.com>
2018-01-31 18:39:03 +0100

    dz

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,8 +1,32 @@
 import express from "express";
-import User from "../models/Book";
 import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-router.get("/search", (req, res) => {});
+router.get("/search", (req, res) => {
+	res.json({
+		books: [
+			{
+				goodreadsId: 1,
+				title: "1984",
+				authors: "Orwell",
+				covers: [
+					"https://fr.cdn.v5.futura-sciences.com/buildsv6/images/wide1920/8/5/4/854c15a2cc_100124_chameau-dromadaire-difference-01.jpg",
+					"http://resize-parismatch.ladmedia.fr/img/var/news/storage/images/paris-match/actu/faits-divers/un-ado-mordu-et-pietine-par-un-chameau-sur-le-chemin-de-l-ecole-1208516/20459249-1-fre-FR/Un-ado-mordu-et-pietine-par-un-chameau-sur-le-chemin-de-l-ecole.jpg"
+				],
+				pages: 198
+			},
+			{
+				goodreadsId: 2,
+				title: "salu",
+				authors: "dsqsd",
+				covers: [
+					"https://fr.cdn.v5.futura-sciences.com/buildsv6/images/wide1920/8/5/4/854c15a2cc_100124_chameau-dromadaire-difference-01.jpg",
+					"http://resize-parismatch.ladmedia.fr/img/var/news/storage/images/paris-match/actu/faits-divers/un-ado-mordu-et-pietine-par-un-chameau-sur-le-chemin-de-l-ecole-1208516/20459249-1-fre-FR/Un-ado-mordu-et-pietine-par-un-chameau-sur-le-chemin-de-l-ecole.jpg"
+				],
+				pages: 198
+			}
+		]
+	});
+});
 export default router;
